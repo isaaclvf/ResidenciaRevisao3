873575108430071c715193d8f03c190e1f6dcdd4
Isaac Farias <isaacfarias006@gmail.com>
2022-11-29 22:36:10 -0300

    Adiciona exercício 3

---
+++ Exercicio3/IsArmstrong.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    public static class Armstrong
+    {
+        public static bool IsArmstrong(this int i)
+        {
+            // https://www.javatpoint.com/armstrong-number-in-c
+            int n = i;
+            int tmp = n;
+            int sum = 0;
+            int r;
+
+            while (n > 0)
+            {
+                r = n % 10;
+                sum = sum + (r * r * r);
+                n = n / 10;
+            }
+
+            return (tmp == sum);
+        }
+    }
+}
---
+++ Exercicio3/Program.cs
@@ -0,0 +1,5 @@
+﻿using Exercicio3;
+
+for (int i = 0; i < 1000; i++)
+    if(i.IsArmstrong())
+        Console.WriteLine(i);
